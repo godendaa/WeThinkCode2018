5fbb007578e5146143d22274f0e9beaac57cf984
Gary ODENDAAL <godendaa@e4r24p4.wethinkcode.co.za>
2018-06-06 12:19:55 +0200

    first functions

---
+++ libft/libft.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: godendaa <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/06/06 11:57:02 by godendaa          #+#    #+#             */
+/*   Updated: 2018/06/06 11:58:27 by godendaa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+void	ft_putchar(char c);
+void	ft_putstr(char const *s);
+
+#endif
